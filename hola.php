512ea50c7fbee56117e54a450afcc34e19d7d7ce
martha <martha.michaca@gmail.com>
2014-06-09 14:26:53 -0500

    Agrego ejemplos de variables y tipos

--- hola.php
+++ hola.php
@@ -1,2 +1,51 @@
 <?php
-echo 'hola mundo';
+/**
+comentario de varias lineas
+*/
+
+$message='hola mundo';
+
+echo "$message\n";
+
+define('PUBLISHER', 'Springer');
+
+echo PUBLISHER;
+
+$pi=0.0314E2;
+
+echo "\n$pi\n";
+
+echo '\n$pi';
+
+$people=['Luis', 'Miriam', 'Noe', 1234, false,[2],];
+
+$old= array(2,3,4,);
+
+
+$product=['name' => 'Ajax',
+		  'price' => 230,
+		  'available' =>true,
+         ];
+
+
+
+		 
+echo $product['price'];
+
+echo "\n{$product['name']}\n";
+		 
+print_r($old);
+
+print_r($people);
+
+print_r($product);
+
+$foo= 'bar';
+
+$$foo= 'baz';
+
+echo "\n$foo";
+
+echo "\n$bar";
+
+?>
